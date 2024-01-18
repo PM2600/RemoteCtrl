07123f42a1df0c04b8008a64a50f6275721a05e8
PM2600 <93525669+PM2600@users.noreply.github.com>
2024-01-18 15:11:10 +0800

    屏幕截屏和发送

--- RemoteCtrl/RemoteCtrl/RemoteCtrl.cpp
+++ RemoteCtrl/RemoteCtrl/RemoteCtrl.cpp
@@ -10,6 +10,7 @@
 #include <stdio.h>
 #include <io.h>
 #include <list>
+#include <atlimage.h>
 
 #ifdef _DEBUG
 #define new DEBUG_NEW
@@ -244,6 +245,46 @@ int MouseEvent() {
     return 0;
 }
 
+int SendScreen() {
+    CImage screen;
+    HDC hScreen = ::GetDC(NULL);
+    int nBitPerPixel = GetDeviceCaps(hScreen, BITSPIXEL);
+    int nWidth = GetDeviceCaps(hScreen, HORZRES);
+    int nHeight = GetDeviceCaps(hScreen, VERTRES);
+    screen.Create(nWidth, nHeight, nBitPerPixel);
+    BitBlt(screen.GetDC(), 0, 0, 1920, 1030, hScreen, 0, 0, SRCCOPY);
+    ReleaseDC(NULL, hScreen);
+    HGLOBAL hMem = GlobalAlloc(GMEM_MOVEABLE, 0);
+    if (hMem == NULL) {
+        return -1;
+    }
+
+    IStream* pStream = NULL;
+    HRESULT ret = CreateStreamOnHGlobal(hMem, TRUE, &pStream);
+    if (ret == S_OK) {
+        screen.Save(pStream, Gdiplus::ImageFormatPNG);
+        LARGE_INTEGER bg = { 0 };
+        pStream->Seek(bg, STREAM_SEEK_SET, NULL);
+        PBYTE pData = (PBYTE)GlobalLock(hMem);
+        SIZE_T nSize = GlobalSize(hMem);
+        CPacket pack(6, pData, nSize);
+        CServerSocket::getInstance()->Send(pack);
+        GlobalUnlock(hMem);   
+    }
+
+    //DWORD tick = GetTickCount64();
+    //screen.Save(_T("test2024.png"), Gdiplus::ImageFormatPNG);
+    //TRACE("png: %d\r\n", GetTickCount64() - tick);
+    //DWORD tick = GetTickCount64();
+    //screen.Save(_T("test2024.jpg"), Gdiplus::ImageFormatJPEG);
+    //TRACE("jpg: %d\r\n", GetTickCount64() - tick);
+
+    pStream->Release();
+    GlobalFree(hMem);
+    screen.ReleaseDC();
+    return 0;
+}
+
 int main()
 {
     int nRetCode = 0;
@@ -280,7 +321,7 @@ int main()
             //    int ret = perver->DealCommand();
             //}
             //// TODO
-            int nCmd = 1;
+            int nCmd = 6;
             switch (nCmd) {
             case1: // 查看磁盘分区
                 MakeDiverInfo();
@@ -296,6 +337,9 @@ int main()
                 break;
             case 5: // 鼠标操作
                 MouseEvent();
+                break;
+            case 6: // 发送屏幕内容 --> 发送屏幕的截图
+                SendScreen();
                 break;
             }
         }
