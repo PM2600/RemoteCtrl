652badaebbb39df5997bdd8fcd1fb69fedd1d415
PM2600 <3262929659@qq.com>
2024-01-23 21:14:53 +0800

    监视窗口

---
+++ RemoteCtrl/RemoteClient/WatchDialog.cpp
@@ -0,0 +1,34 @@
+﻿// WatchDialog.cpp: 实现文件
+//
+
+#include "pch.h"
+#include "RemoteClient.h"
+#include "WatchDialog.h"
+#include "afxdialogex.h"
+
+
+// CWatchDialog 对话框
+
+IMPLEMENT_DYNAMIC(CWatchDialog, CDialog)
+
+CWatchDialog::CWatchDialog(CWnd* pParent /*=nullptr*/)
+	: CDialog(IDD_DLG_WATCH, pParent)
+{
+
+}
+
+CWatchDialog::~CWatchDialog()
+{
+}
+
+void CWatchDialog::DoDataExchange(CDataExchange* pDX)
+{
+	CDialog::DoDataExchange(pDX);
+}
+
+
+BEGIN_MESSAGE_MAP(CWatchDialog, CDialog)
+END_MESSAGE_MAP()
+
+
+// CWatchDialog 消息处理程序
