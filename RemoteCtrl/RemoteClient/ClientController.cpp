0cbeede861d77f0293721e02d68031307613d96e
PM2600 <93525669+PM2600@users.noreply.github.com>
2024-02-02 20:21:29 +0800

    代码重构

--- RemoteCtrl/RemoteClient/ClientController.cpp
+++ RemoteCtrl/RemoteClient/ClientController.cpp
@@ -1,6 +1,9 @@
 #include "pch.h"
 #include "ClientController.h"
 
+std::map<UINT, CClientController::MSGFUNC> CClientController::m_mapFunc;
+CClientController* CClientController::m_instance;
+
 CClientController* CClientController::getInstance()
 {
 	if (m_instance == NULL) {
@@ -11,7 +14,7 @@ CClientController* CClientController::getInstance()
 			{WM_SEND_DATA, &CClientController::OnSendData},
 			{WM_SHOW_STATUS, &CClientController::OnShowStatus},
 			{WM_SHOW_WATCH, &CClientController::OnShowWatch},
-			{-1, NULL}
+			{(UINT) - 1, NULL}
 		};
 		for (int i = 0; MsgFuncs[i].func != NULL; i++) {
 			m_mapFunc.insert(std::pair<UINT, MSGFUNC>(MsgFuncs[i].nMsg, MsgFuncs[i].func));
@@ -23,7 +26,7 @@ CClientController* CClientController::getInstance()
 int CClientController::InitController()
 {
 	m_hThread = (HANDLE)_beginthreadex(NULL, 0, &CClientController::threadEntry, this, 0, &m_nThreadID);
-
+	m_statusDlg.Create(IDD_DLG_STATUS, &m_remoteDlg);
 	return 0;
 }
 
@@ -35,12 +38,14 @@ int CClientController::Invoke(CWnd*& pMainWnd)
 
 LRESULT CClientController::SendMessage(MSG msg)
 {
-	UUID uuid;
-	UuidCreate(&uuid);
-	auto pr = m_mapMessage.insert(std::pair<UUID, MSG>(uuid, msg));
-
-	PostThreadMessage(m_nThreadID, WM_SEND_MESSAGE, (WPARAM)&pr.second, (LPARAM)&pr.first);
-	return LRESULT();
+	HANDLE hEvent = CreateEvent(NULL, TRUE, FALSE, NULL);
+	if (hEvent == NULL) {
+		return -2;
+	}
+	MSGINFO info(msg);
+	PostThreadMessage(m_nThreadID, WM_SEND_MESSAGE, (WPARAM)&info, (LPARAM)hEvent);
+	WaitForSingleObject(hEvent, -1);
+	return info.result;
 }
 
 void CClientController::threadFunc()
@@ -50,16 +55,16 @@ void CClientController::threadFunc()
 		TranslateMessage(&msg);
 		DispatchMessage(&msg);
 		if (msg.message == WM_SEND_MESSAGE) {
-			MSG* pmsg = (MSG*)msg.wParam;
-			UUID* puuid = (UUID*)msg.lParam;
-			std::map<UINT, MSGFUNC>::iterator it = m_mapFunc.find(pmsg->message);
+			MSGINFO* pmsg = (MSGINFO*)msg.wParam;
+			HANDLE hEvent = (HANDLE)msg.lParam;
+			std::map<UINT, MSGFUNC>::iterator it = m_mapFunc.find(msg.message);
 			if (it != m_mapFunc.end()) {
-				(this->*it->second)(pmsg->message, pmsg->wParam, pmsg->lParam);
-				std::map<UUID, MSG>::iterator it = m_mapMessage.find(*puuid);
-				if (it != m_mapMessage.end()) {
-					m_mapMessage.erase(*puuid);
-				}
+				pmsg->result = (this->*it->second)(pmsg->msg.message, pmsg->msg.wParam, pmsg->msg.lParam);								
 			}
+			else {
+				pmsg->result = -1;
+			}
+			SetEvent(hEvent);
 		}
 		else {
 			std::map<UINT, MSGFUNC>::iterator it = m_mapFunc.find(msg.message);
@@ -76,4 +81,24 @@ unsigned __stdcall CClientController::threadEntry(void* arg)
 	thiz->threadFunc();
 	_endthreadex(0);
 	return 0;
+}
+
+LRESULT CClientController::OnSendPack(UINT nMsg, WPARAM wParam, LPARAM lParam)
+{
+	return LRESULT();
+}
+
+LRESULT CClientController::OnSendData(UINT nMsg, WPARAM wParam, LPARAM lParam)
+{
+	return LRESULT();
+}
+
+LRESULT CClientController::OnShowStatus(UINT nMsg, WPARAM wParam, LPARAM lParam)
+{
+	return m_statusDlg.ShowWindow(SW_SHOW);
+}
+
+LRESULT CClientController::OnShowWatch(UINT nMsg, WPARAM wParam, LPARAM lParam)
+{
+	return m_watchDlg.DoModal();
 }
--- RemoteCtrl/RemoteClient/ClientController.h
+++ RemoteCtrl/RemoteClient/ClientController.h
@@ -4,6 +4,7 @@
 #include "RemoteClientDlg.h"
 #include "StatusDlg.h"
 #include <map>
+#include "Resource.h"
 
 #define WM_SEND_PACK (WM_USER + 1)   // 发送包数据
 #define WM_SEND_DATA (WM_USER + 2)   // 发送数据
@@ -21,8 +22,9 @@ public:
 	LRESULT SendMessage(MSG msg);
 
 protected:
-	CClientController() {
-
+	CClientController():m_statusDlg(&m_remoteDlg), m_watchDlg(&m_remoteDlg){
+		m_hThread = INVALID_HANDLE_VALUE;
+		m_nThreadID = -1;
 	}
 	~CClientController() {
 		WaitForSingleObject(m_hThread, 100);
@@ -44,10 +46,29 @@ protected:
 
 
 private:
+	typedef struct MsgInfo{
+		MSG msg;
+		LRESULT result;
+		MsgInfo(MSG m) {
+			result = 0;
+			memcpy(&msg, &m, sizeof(MSG));
+		}
+
+		MsgInfo(const MsgInfo& m) {
+			result = m.result;
+			memcpy(&msg, &m.msg, sizeof(MSG));
+		}
+		MsgInfo& operator=(const MsgInfo& m) {
+			if (this != &m) {
+				result = m.result;
+				memcpy(&msg, &m.msg, sizeof(MSG));
+			}
+			return *this;
+		}
+	}MSGINFO;
+
 	typedef LRESULT(CClientController::* MSGFUNC)(UINT nMsg, WPARAM wParam, LPARAM lParam);
 	static std::map<UINT, MSGFUNC> m_mapFunc;
-	std::map<UUID, MSG> m_mapMessage;
-
 	CWatchDialog m_watchDlg;
 	CRemoteClientDlg m_remoteDlg;
 	CStatusDlg m_statusDlg;
