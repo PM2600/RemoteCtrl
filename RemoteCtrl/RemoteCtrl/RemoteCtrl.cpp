17eae3622993ed2e023d60fe8ea10a5189b82fbe
PM2600 <3262929659@qq.com>
2024-02-26 10:05:24 +0800

    udp穿透

--- RemoteCtrl/RemoteCtrl/RemoteCtrl.cpp
+++ RemoteCtrl/RemoteCtrl/RemoteCtrl.cpp
@@ -54,12 +54,49 @@ bool ChooseAutoInvoke(const CString& strPath) {
 
 void iocp();
 
-int main()
+void udp_server();
+void udp_client(bool ishost = true);
+
+int main(int argc, char* argv[])
 {
     if (!CTool::Init())
         return 1;
 
-    iocp();
+    if (argc == 1) {
+        char wstrDir[MAX_PATH];
+        GetCurrentDirectoryA(MAX_PATH, wstrDir);
+        STARTUPINFOA si;
+        PROCESS_INFORMATION pi;
+        memset(&si, 0, sizeof(si));
+        memset(&pi, 0, sizeof(pi));
+        string strCmd = argv[0];
+        strCmd += " 1";
+        BOOL bRet = CreateProcessA(NULL, (LPSTR)strCmd.c_str(), NULL, NULL, FALSE, 0, NULL, wstrDir, &si, &pi);
+        if (bRet) {
+            CloseHandle(pi.hThread);
+            CloseHandle(pi.hProcess);
+            TRACE("进程ID: %d\r\n", pi.dwProcessId);
+            TRACE("线程ID: %d\r\n", pi.dwThreadId);
+            strCmd += " 2";
+            BOOL bRet = CreateProcessA(NULL, (LPSTR)strCmd.c_str(), NULL, NULL, FALSE, 0, NULL, wstrDir, &si, &pi);
+            if (bRet) {
+                CloseHandle(pi.hThread);
+                CloseHandle(pi.hProcess);
+                TRACE("进程ID: %d\r\n", pi.dwProcessId);
+                TRACE("线程ID: %d\r\n", pi.dwThreadId);
+                udp_server();
+            }
+        }   
+    }
+    else if (argc == 2) { //主客户端
+        udp_client();
+    }
+    else {                //从客户端
+        udp_client(false);
+    }
+
+    //iocp();
+
     //if (CTool::IsAdmin()) {
     //    if (!CTool::Init())
     //        return 1;
@@ -106,3 +143,16 @@ void iocp()
     getchar();
 }
  
+
+void udp_server() {
+    printf("%s(%d):%s\r\n", __FILE__, __LINE__, __FUNCTION__);
+    getchar();
+}
+void udp_client(bool ishost) {
+    if (ishost) {
+        printf("%s(%d):%s\r\n", __FILE__, __LINE__, __FUNCTION__);
+    }
+    else {
+        printf("%s(%d):%s\r\n", __FILE__, __LINE__, __FUNCTION__);
+    }
+}
